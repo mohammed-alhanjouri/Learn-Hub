326cf72a69bb9d5ed90a6e3dfa3d60f2efa19ff9
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-08-06 15:14:21 +0300

    Import Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+
+      
     </div>
   )
 }
