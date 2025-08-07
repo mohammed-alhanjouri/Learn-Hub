25efb5e3a4dc2f5632360be72d5e467052aebf1a
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-08-07 23:19:16 +0300

    Responsive Design for Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import './Navbar.css'
-// import logo from '../../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import { faGraduationCap, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [isColored, setIsColored] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleScroll = () => {
     window.scrollY > 50 ? setIsColored(true) : setIsColored(false);
@@ -23,16 +23,20 @@ const Navbar = () => {
     <nav className={`navbar ${isColored ? 'colored-nav' : ''}`}>
       <div className="logo">
         <FontAwesomeIcon icon={faGraduationCap} className="icon" size='2x' />
-        {/* <img src={logo} alt="LearnHub Logo" /> */}
         <h1>LearnHub</h1>
       </div>
-      <ul>
-        <li> <Link to='hero' smooth={true} offset={0} duration={500}> Home </Link></li>
-        <li> <Link to='programs' smooth={true} offset={-260} duration={500}> Program </Link></li>
-        <li> <Link to='about' smooth={true} offset={-150} duration={500}> About Us </Link></li>
-        <li> <Link to='campus' smooth={true} offset={-260} duration={500}> Campus </Link></li>
-        <li> <Link to='testimonials' smooth={true} offset={-260} duration={500}> Testimonials </Link></li>
-        <li> <Link to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact Us</button> </Link></li>
+
+      <div className="mobile-menu-toggle" onClick={() => setIsMobile(!isMobile)}>
+        <FontAwesomeIcon icon={isMobile ? faTimes : faBars} />
+      </div>
+
+      <ul className={`nav-menu ${isMobile ? 'active' : ''}`}>
+        <li> <Link to='hero' smooth={true} offset={0} duration={500} onClick={() => setIsMobile(false)} > Home </Link></li>
+        <li> <Link to='programs' smooth={true} offset={-260} duration={500} onClick={() => setIsMobile(false)} > Program </Link></li>
+        <li> <Link to='about' smooth={true} offset={-150} duration={500} onClick={() => setIsMobile(false)} > About Us </Link></li>
+        <li> <Link to='campus' smooth={true} offset={-260} duration={500} onClick={() => setIsMobile(false)} > Campus </Link></li>
+        <li> <Link to='testimonials' smooth={true} offset={-260} duration={500} onClick={() => setIsMobile(false)} > Testimonials </Link></li>
+        <li> <Link to='contact' smooth={true} offset={-260} duration={500} onClick={() => setIsMobile(false)} ><button className='btn'>Contact Us</button> </Link></li>
       </ul>
     </nav>
   )
