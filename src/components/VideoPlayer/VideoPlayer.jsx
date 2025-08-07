71665f094a5b968755e4d066ad4506f375a2a9dc
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-08-07 15:50:23 +0300

    Add keyboard support for VideoPlayer

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import './VideoPlayer.css'
 import video from '../../assets/about-video.mp4'
 
@@ -12,6 +12,23 @@ const VideoPlayer = ({playState, setPlayState}) => {
             setPlayState(false);
         }
     }
+
+    // Add keyboard support
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.key === 'Escape' && playState) {
+                setPlayState(false);
+            }
+        };
+
+        if (playState) {
+            document.addEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [playState, setPlayState]);
 
   return (
     <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
